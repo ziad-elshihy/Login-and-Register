56ff3337d97a4658f790ffaca13fa516514446d2
ziad-elshihy <elshihyziad@gmail.com>
2023-08-09 13:41:15 +0300

    ..

--- Routs/user.cjs
+++ Routs/user.cjs
@@ -5,6 +5,9 @@ const router = express.Router()
 
 router.post('/api/users/register', userController.register)
 router.post('/api/users/login', userController.login)
+router.get('/api/users/ziad', (req,res)=>{
+   res.json({msg: 'Hello Ziad'})
+})
 
 
 module.exports = router
